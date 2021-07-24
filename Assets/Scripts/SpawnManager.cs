b3e6f09d849ce26fe156450c51f214b7ecc76b95
hahas507 <aq12361@gmail.com>
2021-07-24 13:21:32 +0900

    3.1.8
    Create a spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
