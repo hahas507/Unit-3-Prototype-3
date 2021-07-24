8f2a497aeaf56fc5fd789d6bfb2172c28cd4606c
hahas507 <aq12361@gmail.com>
2021-07-24 13:16:05 +0900

    3.1.7
    Make an obstacle and move it left

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 30;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
