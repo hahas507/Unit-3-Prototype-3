a9a242977336fcd8cf47efb1faa406c5d7c34c1c
hahas507 <aq12361@gmail.com>
2021-07-24 13:38:01 +0900

    3.2.2
    Reset position of background.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos;
+
     private void Start()
     {
+        startPos = transform.position;
     }
 
     private void Update()
     {
+        if (transform.position.x < startPos.x - 56)
+        {
+            transform.position = startPos;
+        }
     }
 }
